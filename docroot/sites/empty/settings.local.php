07f80c58a1e64780fa14df5488a19094b8718352
Karl Kedrovsky <karl.kedrovsky@vml.com>
2018-09-23 17:57:14 -0500

    updated demo data and paragraphs

---
+++ docroot/sites/empty/settings.local.php
@@ -0,0 +1,53 @@
+<?php
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/empty/services.local.yml';
+
+$config['system.logging']['error_level'] = 'verbose';
+
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+$settings['extension_discovery_scan_tests'] = TRUE;
+
+$settings['rebuild_access'] = TRUE;
+
+$settings['skip_permissions_hardening'] = TRUE;
+
+$settings['twig_tweak_check_access'] = FALSE;
+
+$databases['default']['default'] = array (
+  'database' => 'empty',
+  'username' => 'user',
+  'password' => 'user',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+# Workaround for permission issues with NFS shares
+$settings['file_chmod_directory'] = 0777;
+$settings['file_chmod_file'] = 0666;
+
+# File system settings.
+$config['system.file']['path']['temporary'] = '/tmp';
+
+# Reverse proxy configuration (Docksal's vhost-proxy)
+if (PHP_SAPI !== 'cli') {
+  $settings['reverse_proxy'] = TRUE;
+  $settings['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
+  // HTTPS behind reverse-proxy
+  if (
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' &&
+    !empty($settings['reverse_proxy']) && in_array($_SERVER['REMOTE_ADDR'], $settings['reverse_proxy_addresses'])
+  ) {
+    $_SERVER['HTTPS'] = 'on';
+    // This is hardcoded because there is no header specifying the original port.
+    $_SERVER['SERVER_PORT'] = 443;
+  }
+}
--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,3 +1,5 @@
 <?php
 
 $sites['paragraphs.drupalcorn2018.docksal'] = 'paragraphs';
+$sites['bricks.drupalcorn2018.docksal'] = 'bricks';
+$sites['empty.drupalcorn2018.docksal'] = 'empty';
